f745fc46cf968059ffff3ce7812aa871e2d7a63b
nguyenthuanit265 <nguyenthuanit265@gmail.com>
2024-07-19 15:41:17 +0700

    commit API

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Web3 = require('web3');
 require('dotenv').config();
+const NodeCache = require('node-cache');
 
 const app = express();
 const port = 4000;
@@ -8,102 +9,172 @@ const port = 4000;
 // Connect to the Sepolia testnet using Infura
 const web3 = new Web3(new Web3.providers.HttpProvider(`https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`));
 
-// ABI of your deployed contract
-// const abi = [
-//     {
-//         "inputs": [],
-//         "name": "getRandomNumber",
-//         "outputs": [
-//             {
-//                 "internalType": "uint256",
-//                 "name": "",
-//                 "type": "uint256"
-//             }
-//         ],
-//         "stateMutability": "view",
-//         "type": "function"
-//     }
-// ];
+//const abi = [
+//	{
+//		"inputs": [],
+//		"name": "rollDice",
+//		"outputs": [
+//			{
+//				"internalType": "uint256",
+//				"name": "",
+//				"type": "uint256"
+//			}
+//		],
+//		"stateMutability": "view",
+//		"type": "function"
+//	}
+//];
 
 const abi = [
-	{
-		"inputs": [],
-		"name": "rollDice",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	}
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "roller",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "result",
+        "type": "uint256"
+      }
+    ],
+    "name": "DiceRolled",
+    "type": "event"
+  },
+  {
+    "inputs": [],
+    "name": "rollDice",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
 ];
 
 // Contract address
 const contractAddress = process.env.CONTRACT_ADDRESS;
 const contract = new web3.eth.Contract(abi, contractAddress);
 
+// Unlock your account (Replace with your account and private key)
+const account = process.env.ACCOUNT_ADDRESS;
+const privateKey = process.env.PRIVATE_KEY;
+
+// Cache setup
+const cache = new NodeCache({ stdTTL: 600 }); // 10 minutes
+
 // Endpoint to get the random number
+//app.get('/roll-dice', async (req, res) => {
+//    try {
+//        const rollDiceResponse = await contract.methods.rollDice().call();
+//        console.log("rollDiceResponse: ", rollDiceResponse);
+//        res.json({ rollDiceResponse });
+//    } catch (error) {
+//        res.status(500).json({ error: error.message });
+//    }
+//});
+
+// Endpoint to roll dice
 app.get('/roll-dice', async (req, res) => {
-    try {
-        const rollDiceResponse = await contract.methods.rollDice().call();
-        console.log("rollDiceResponse: ", rollDiceResponse);
-        res.json({ rollDiceResponse });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-// Endpoint to get block details
-app.get('/block/:blockNumber', async (req, res) => {
-    const blockNumber = req.params.blockNumber;
-    try {
-        const block = await web3.eth.getBlock(blockNumber);
-        console.log("blockNumber: ", blockNumber, " - ", "block: ", block);
-        res.json(block);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-// Endpoint to get the latest block number
-app.get('/latest-block', async (req, res) => {
-    try {
-        const latestBlockNumber = await web3.eth.getBlockNumber();
-        console.log("latestBlockNumber: ", latestBlockNumber);
-        res.json({ latestBlockNumber });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-app.get('/fetch-blockchain', async (req, res) => {
-  const { startBlock, endBlock } = req.query;
-  const start = parseInt(startBlock, 10) || 0;
-  const end = parseInt(endBlock, 10) || (await web3.eth.getBlockNumber());
-  let blockchainData = [];
-
   try {
-    for (let i = start; i <= end; i++) {
-      const block = await web3.eth.getBlock(i, true); // Fetch block with transaction details
-      blockchainData.push(block);
+    const tx = contract.methods.rollDice();
+    const gas = await tx.estimateGas({ from: account });
+    const gasPrice = await web3.eth.getGasPrice();
+    const data = tx.encodeABI();
+    const nonce = await web3.eth.getTransactionCount(account);
 
-      // Log progress
-      if (i % 10 == 0) {
-        console.log(`Fetched block ${i}`);
-      }
-    }
+    const signedTx = await web3.eth.accounts.signTransaction(
+      {
+        to: contractAddress,
+        data,
+        gas,
+        gasPrice,
+        nonce,
+        chainId: 11155111 // Sepolia testnet chain ID
+      },
+      privateKey
+    );
 
-    res.json(blockchainData);
+    web3.eth.sendSignedTransaction(signedTx.rawTransaction)
+      .on('receipt', async (receipt) => {
+
+        console.log("sendSignedTransaction - receipt: ", receipt);
+        // Decode event logs to get the dice roll result
+        const eventAbi = contract.options.jsonInterface.find((e) => e.name === 'DiceRolled' && e.type === 'event');
+        const event = receipt.logs.find(log => log.address.toLowerCase() === contractAddress.toLowerCase());
+
+        if (event) {
+          const decodedEvent = web3.eth.abi.decodeLog(eventAbi.inputs, event.data, event.topics.slice(1));
+          res.json({ rollDiceResponse: decodedEvent.result, receiptFromSignedTransaction: receipt });
+        } else {
+          res.status(500).json({ error: "Event not found in transaction receipt" });
+        }
+      })
+      .on('error', (error) => {
+        res.status(500).json({ error: error.message });
+      });
+
+
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
+// Endpoint to get block details
+app.get('/block/:blockNumber', async (req, res) => {
+  const blockNumber = req.params.blockNumber;
+  try {
+    const block = await web3.eth.getBlock(blockNumber);
+    console.log("blockNumber: ", blockNumber, " - ", "block: ", block);
+    res.json(block);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Endpoint to fetch blocks containing rollDice transactions with pagination
+app.get('/fetch-roll-dice-blocks', async (req, res) => {
+  // const page = parseInt(req.query.page) || 1;
+  // const pageSize = parseInt(req.query.pageSize) || 1000; // Fetch 1000 blocks at a time
+
+  try {
+    // const cacheKey = `events_${page}_${pageSize}`;
+    // if (cache.has(cacheKey)) {
+    //   return res.json(cache.get(cacheKey));
+    // }
+
+    const events = await contract.getPastEvents('DiceRolled', {
+      fromBlock: 0,
+      toBlock: 'latest'
+    });
+
+    const blocks = new Set();
+    for (let event of events) {
+      blocks.add(event.blockNumber);
+    }
+
+    const blockData = [];
+    for (let blockNumber of blocks) {
+      const block = await web3.eth.getBlock(blockNumber, true); // Fetch block with transaction details
+      blockData.push(block);
+    }
+
+    // cache.set(cacheKey, blockData);
+    res.json(blockData);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
--- vrf_project/contracts/DiceRoller.sol
+++ vrf_project/contracts/DiceRoller.sol
@@ -2,8 +2,12 @@
 pragma solidity ^0.8.0;
 
 contract DiceRoller {
-    function rollDice() public view returns (uint256) {
+    event DiceRolled(address indexed roller, uint256 result);
+
+    function rollDice() public returns (uint256) {
         uint256 random = uint256(keccak256(abi.encodePacked(blockhash(block.number - 1), block.timestamp)));
-        return (random % 6)  + 1;
+        uint256 result = (random % 6) + 1;
+        emit DiceRolled(msg.sender, result);
+        return result;
     }
 }
